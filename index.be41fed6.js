f4b393e343b77bbecf513e2fa302109e460a6379
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-03 17:01:04 +0300

    make build

---
+++ index.be41fed6.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("tr").forEach(function(e){var l=e.cells[1].cloneNode(!0),n=e.cells[e.cells.length-1];e.insertAdjacentElement("beforeend",l),e.insertAdjacentElement("beforeend",n)});
+//# sourceMappingURL=index.be41fed6.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const tableRows = document.querySelectorAll("tr");
+tableRows.forEach((row)=>{
+    const clone = row.cells[1].cloneNode(true);
+    const lastClone = row.cells[row.cells.length - 1];
+    row.insertAdjacentElement("beforeend", clone);
+    row.insertAdjacentElement("beforeend", lastClone);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
